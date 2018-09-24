e9ed33576094d9bcb74c00692027a1a6f6213b34
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2018-09-24 19:13:04 -0300

    Corrigido erros pós pull upstream - retirado as categorias e recomendações

--- app/models/category_recommendation.rb
+++
@@ -1,4 +0,0 @@
-class CategoryRecommendation < ApplicationRecord
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
-  has_many :recommendations, dependent: :destroy
-end
--- spec/factories/category_recommendations.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :category_recommendation do
-    sequence(:name) { |n| "Categoria_#{n}" }
-  end
-end
