790f67350292b909db014a9ca7d26aa7ace80450
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2018-09-24 19:08:29 -0300

    erros pós pull

--- app/models/recommendation.rb
+++
@@ -1,8 +0,0 @@
-class Recommendation < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
-
-  belongs_to :category_recommendation
-
-  mount_uploader :image, RecommendationImageUploader
-end
